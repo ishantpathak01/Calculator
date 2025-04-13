0bb1c538d9a164fabc6a5365f21020163af151fc
ishantpathak01 <ishantpathak25@gmail.com>
2025-04-13 03:39:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let result = "";
+
+document.querySelectorAll("button").forEach(button => {
+  button.addEventListener("click", () => {
+    let value = button.innerText;
+
+    if (value === "=") {
+      result = eval(result);
+    } else if (value === "C") {
+      result = "";
+    } else if (value === "DEL") {
+      result = result.slice(0, -1);
+    } else if (value === "×") {
+      result += "*";
+    } else if (value === "÷") {
+      result += "/";
+    } else {
+      result += value;
+    }
+
+    document.querySelector(".text").value = result;
+  });
+});
